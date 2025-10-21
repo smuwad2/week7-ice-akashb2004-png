5f74b34dc204232dda5513e3b14544aaaa595d54
akashb2004-png <akash.b.2024@computing.smu.edu.sg>
2025-10-21 02:44:24 +0000

    second-commit

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -3,16 +3,32 @@
         // Add code in appropriate places
         data() {
             return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
+                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+                newItem: ''
             }
-            
+        },
+        methods : {
+            addItem(){
+                if (this.newItem.trim() !== ''){
+                    this.items.push(this.newItem)
+                    this.newItem = ''
+                }
+            },
+            removeItem(index){
+                this.items.splice(index,1)
+            }
         }
     }
 </script>
 
 <template>
 
-    <h2>Shopping Cart</h2>
-    <!-- TODO: Add Code Here -->
+    <h2>Wendy's roster</h2>
+    <!-- TODO: Add Code here -->
+    <ul>
+      <li v-for="(item, index) in items" v-bind:key="item"> {{ item }} <button @click="removeItem(index)">Remove</button></li>  
+    </ul> 
+    <input v-model = "newItem" placeholder="Add new item" />
+    <button @click= "addItem">Add</button>
    
 </template>
